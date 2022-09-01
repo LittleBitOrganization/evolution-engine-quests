1bd28d77cee98acf22cdb55add3447e81aff3292
ariatophanes <ariatophanes@gmail.com>
2022-09-01 16:24:37 +0300

    added serializable attribute

--- Runtime/LittleBitGames/QuestsModule/Trackers/Collections/AchievementsContainerMemento.cs
+++ Runtime/LittleBitGames/QuestsModule/Trackers/Collections/AchievementsContainerMemento.cs
@@ -1,8 +1,10 @@
+using System;
 using LittleBit.Modules.CoreModule;
 using LittleBitGames.QuestsModule.Trackers.Models;
 
 namespace LittleBitGames.QuestsModule.Trackers.Collections
 {
+    [Serializable]
     public class AchievementsContainerMemento : Data
     {
         public readonly SerializableDictionary<string, AchievementSlot> Slots;
