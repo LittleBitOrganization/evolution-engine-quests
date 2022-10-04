6f0c83efefcaf8cb42caa90ab0347112d8caf080
ariatophanes <ariatophanes@gmail.com>
2022-10-04 14:30:56 +0300

    Fixed quest progress

--- Runtime/LittleBitGames/QuestsModule/Quests/Controllers/QuestProgressExtensions.cs
+++ Runtime/LittleBitGames/QuestsModule/Quests/Controllers/QuestProgressExtensions.cs
@@ -3,6 +3,6 @@ namespace LittleBitGames.QuestsModule.Quests.Controllers
     public static class QuestProgressExtensions
     {
         public static bool HasGoalBeenReached(this QuestProgress p)
-            => p.TargetValue >= p.CurrentValue;
+            => p.CurrentValue >= p.TargetValue;
     }
 }
